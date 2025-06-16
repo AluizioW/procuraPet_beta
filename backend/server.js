77053c493cc5323808e26a205100f4879c2cb1dd
AluizioW <aluizio.sousa@discente.ufopa.edu.br>
2025-06-16 16:29:46 -0300

    backend v0.1

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3210;
+
+app.get('/', (req, res) => {
+  res.json([
+    {
+        // id: crypto.randomUUID(),
+        id: '1',
+        title: 'ProcuraPet',
+        description: 'Encontre o pet perdido ou ajude a encontrar um pet perdido.',
+        image: 'https://http.cat/images/100.jpg'
+    },
+
+    {
+        // id: crypto.randomUUID(),
+        id: '2',
+        title: 'Sobre',
+        description: 'ProcuraPet é uma plataforma para ajudar a encontrar pets perdidos.',
+        image: 'https://http.cat/status/200.jpg'
+    },
+
+    {
+        // id: crypto.randomUUID(),
+        id: '3',
+        title: 'Contato',
+        description: 'Entre em contato conosco para mais informações.',
+        image: 'https://http.cat/images/102.jpg'
+    }
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
