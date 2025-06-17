0bccf657e1dabeb945988f3d0ca13c2176e013cd
AluizioW <aluizio.sousa@discente.ufopa.edu.br>
2025-06-17 11:20:49 -0300

    nav bar v0.2

--- frontend/App.js
+++ frontend/App.js
@@ -6,13 +6,34 @@ import DetailsScreen from './src/screens/DetailsScreen';
 // import LoginScreen from './src/screens/LoginScreen';
 // import RegisterScreen from './src/screens/RegisterScreen';
 
+import { Ionicons } from '@expo/vector-icons';
+
 
 const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Details') {
+              iconName = focused ? 'person' : 'person-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#66997B',
+          tabBarInactiveTintColor: 'gray',
+          headerShown: false,
+          })}
+          >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Details" component={DetailsScreen} />
         {/* <Tab.Screen name="Login" component={LoginScreen} />
