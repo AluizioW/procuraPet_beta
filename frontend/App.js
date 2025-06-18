a218f2aa30e24535ee00ef08a8034a2087d58450
AluizioW <aluizio.sousa@discente.ufopa.edu.br>
2025-06-18 08:25:27 -0300

    navbar v0.3

--- frontend/App.js
+++ frontend/App.js
@@ -3,10 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen'; 
 import DetailsScreen from './src/screens/DetailsScreen';
+import NotificationsScreen from './src/screens/NotificationsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 // import LoginScreen from './src/screens/LoginScreen';
 // import RegisterScreen from './src/screens/RegisterScreen';
 
 import { Ionicons } from '@expo/vector-icons';
+import NewPostScreen from './src/screens/DetailsScreen';
 
 
 const Tab = createBottomTabNavigator();
@@ -20,22 +23,27 @@ function App() {
             let iconName;
 
             if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Details') {
-              iconName = focused ? 'person' : 'person-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            }
+              iconName = focused ? 'paw' : 'paw-outline';
+            } else if (route.name === 'NewPost') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            } else if (route.name === 'Notifications') {
+              iconName = focused ? 'notifications' : 'notifications-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            } 
 
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: '#66997B',
           tabBarInactiveTintColor: 'gray',
           headerShown: false,
+          
           })}
           >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Details" component={DetailsScreen} />
+        <Tab.Screen name="NewPost" component={NewPostScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
         {/* <Tab.Screen name="Login" component={LoginScreen} />
         <Tab.Screen name="Register" component={RegisterScreen} /> */}
       </Tab.Navigator>
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-export default function NewPostScreen() {
+export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text>Novo Post</Text>
+      <Text>Meu perfil </Text>
     </View>
   );
 }
